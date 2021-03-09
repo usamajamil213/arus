fa85eed63eff6d9ea708a7ecf19bebdcd94f6e27
Usama Jamil khan <67308766+usamajamil213@users.noreply.github.com>
2021-03-09 15:09:23 +0500

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -31,6 +31,6 @@ Route::post('forgetPassword', 'API\AuthController@forget_password');
 Route::post('getcompanies', 'Company\api\ApiController@get_companies');
 Route::post('getSkills', 'Skills\api\ApiController@get_skills');
 Route::post('getSkillsCategory', 'Skills\api\ApiController@get_skills_cat');
-Route::post('getCategories ', 'Skills\api\ApiController@get_cat');
+Route::post('getCategories', 'Skills\api\ApiController@get_cat');
 Route::post('getProviders', 'Provider\api\ProviderApiController@get_providers');
 
